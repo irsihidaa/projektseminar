0dd4227506834abeed872c58fb138df9c29a7a29
irsihidaa <ihida@uet.edu.al>
2025-05-10 12:03:45 +0000

    hashimi dhe jwt

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/tasks', require('./routes/taskRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+
 
 app.use(errorHandler);
 
